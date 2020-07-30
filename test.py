cd758ce008df76fab267308b2a3ba6797ea954cd
izonejangwonyoung <2000shim@naver.com>
2020-07-30 21:40:32 +0900

    final version

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 #세 정수를 입력받아 최댓값 구하기
 
 print("세 정수의 최댓값을 구합니다.")
-a=int(input("첫 번째 정수의 값을 입력하세요.:"))
+a=int(input("첫 번째 정수의 값을 입력하세요:"))
 
-b=int(input("드 번째 정수의 값을 입력하세요.:"))
+b=int(input("드 번째 정수의 값을 입력하세요:"))
 
-c=int(input("세 번째 정수의 값을 입력하세요.:"))
+c=int(input("세 번째 정수의 값을 입력하세요:"))
 
 
 
 max=a
-if b>max;
+if b>max:
     max=b
-if c>max;
+if c>max:
     max=c
+
+print("주어진 정수 중 최댓값은",max,"입니다")
