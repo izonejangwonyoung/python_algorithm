76c0b617196b8bfe75c4714180724cc9dc3686b8
izonejangwonyoung <2000shim@naver.com>
2020-07-30 21:37:54 +0900

    ..

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print("ronaldo")
-print("messi")
+#세 정수를 입력받아 최댓값 구하기
+
+print("세 정수의 최댓값을 구합니다.")
+a=int(input("첫 번째 정수의 값을 입력하세요.:"))
+
+b=int(input("드 번째 정수의 값을 입력하세요.:"))
+
+c=int(input("세 번째 정수의 값을 입력하세요.:"))
+
+
+
+max=a
+if b>max;
+    max=b
+if c>max;
+    max=c
