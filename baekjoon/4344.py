537a5f3a64de17c13979b5e1ffdf182a0f67917b
izonejangwonyoung <2000shim@naver.com>
2020-09-24 15:08:54 +0900

    단순한 코드오류

---
+++ baekjoon/4344.py
@@ -0,0 +1,51 @@
+
+# 문제
+
+# 대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+# 입력
+
+# 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+# 둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+# 출력
+
+# 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+
+import sys
+def calc_avg():
+    N=int(sys.stdin.readline())
+    
+    
+
+    for i in range(N):
+        
+        list1=list(map(int, sys.stdin.readline().split(' ')))
+        
+
+        avg=sum(list1[1:])/list1[0]
+        count=0
+
+        for j in list1[1:]:
+            if j>avg:
+                count+=1
+
+        
+        print(str("%.3f" %round((count/list1[0])*100, 3))+"%")
+
+calc_avg()
+
+
+
+
+# C = int(input())
+# for i in range(C):
+#     N = list(map(int, input().split(' ')))
+#     avg = sum(N[1:]) / N[0]
+#     cnt = 0
+#     for j in N[1:]:
+#         if j > avg : 
+#             cnt += 1
+            
+#     print(str("%.3f" %round((cnt/N[0])*100, 3))+"%")
+
+
